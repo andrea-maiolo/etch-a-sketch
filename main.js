d1a6e1ac9be7c4fb070b71319454529957510bfc
Andrea Maiolo <maiolo.m.2@gmail.com>
2020-11-07 13:02:51 +0100

    refractoreted the functions so now are more clean and lee pron to errors, working on the random color

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
+//selecting buttons from html
 const reset = document.querySelector('#Reset');
 const toggle = document.querySelector('#toggle');
 const rainbow = document.querySelector('#rainbow');
 const fine = document.querySelector('#fine');
 const pencil = document.querySelector('#black');
 const erase = document.querySelector('#erase');
-var randomColor = false;
-var blackColor = false;
-var eraseColor = false;
-var container = document.querySelector('#container');
-let buttonsDiv = document.querySelector('.buttons');
-let main = document.querySelector('.content');
+const container = document.querySelector('#container');
+const buttonsDiv = document.querySelector('.buttons');
+const content = document.querySelector('.content');
+
+//creating the grid
 for (let i = 0; i < 256; i++) {
 	container.appendChild(document.createElement('div'))
 }
-var divs = document.querySelectorAll('div');
+
+//adding classes
+const divs = document.querySelectorAll('div');
 divs.forEach((div) => {
 	div.classList.add("gridItem");
 	div.classList.add("borderGrid")
@@ -21,16 +23,106 @@ divs.forEach((div) => {
 	container.classList.remove("borderGrid");
 	buttonsDiv.classList.remove('gridItem');
 	buttonsDiv.classList.remove('borderGrid');
-	main.classList.remove('gridItem');
-	main.classList.remove('borderGrid');
-})
-container.style.gridTemplateColumns = "repeat(16, 1fr)";
-container.style.gridTemplateRows = "repeat(16, 1fr)";
-var gridItem = document.querySelectorAll('.gridItem');
-gridItem.forEach((div) => {
-	div.addEventListener('mouseenter', colorIn);
+	content.classList.remove('gridItem');
+	content.classList.remove('borderGrid');
 })
 
+//making the grid a grid(if it makes sense)
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
+container.style.gridTemplateRows = "repeat(16, 1fr)";
+
+//creting the event listener for drawing on the grid
+let gridItem = document.querySelectorAll('.gridItem');
+gridItem.forEach((div) => {
+	div.addEventListener('mouseenter', colorIn);   //this could be modified so that you need to press and hold, look web bos for canvas
+})
+
+//this is the function that add the initial color
+function colorIn(e) {
+	this.classList.remove('rainbow');
+	this.classList.remove('fine');
+	this.classList.remove('erase');
+	this.classList.add("change");
+}
+
+//adding event listener to the buttons with relative functions
+//this is for the rainbow color
+//event listener
+rainbow.addEventListener('click', (e) => {
+	gridItem.forEach((div)=>{
+		div.removeEventListener("mouseenter", colorIn);
+		div.removeEventListener("mouseenter", eraseElement);
+	})
+	gridItem.forEach((div) => {
+		div.addEventListener('mouseenter', random);
+	})
+})
+//function
+function random(e) {
+	this.classList.add('rainbow');
+	this.classList.remove('fine');
+	this.classList.remove('erase');
+	this.classList.remove("change");
+	
+	let myColor = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+	this.style.backgroundColor = myColor;
+}
+
+//this is the eraser
+//event
+erase.addEventListener('click', (e) => {
+	gridItem.forEach((div)=>{
+		div.removeEventListener("mouseenter", colorIn);
+		div.removeEventListener("mouseenter", random);
+	})
+	gridItem.forEach((div) => {
+		div.addEventListener('mouseenter', eraseElement)
+	})
+})
+//function
+function eraseElement(e) {
+	this.classList.remove('rainbow');
+	this.classList.remove('fine');
+	this.classList.add('erase');
+	this.classList.remove("change");
+}
+
+//this is the black button 
+//event
+pencil.addEventListener('click', (e) => {
+	gridItem.forEach((div)=>{
+		div.removeEventListener("mouseenter", eraseElement);
+		div.removeEventListener("mouseenter", random);
+	})
+	gridItem.forEach((div) => {
+		div.addEventListener('mouseenter', colorIn)
+	})
+})
+
+//this toggle the grid borders
+toggle.addEventListener('click', (e) => {
+	gridItem.forEach((div) => {
+		div.classList.toggle('borderGrid')
+	})
+})
+
+
+//this is the fine drawing 
+//event
+fine.addEventListener('click', (e) => {
+	gridItem.forEach((div) => {
+		div.addEventListener('mouseenter', pressure)
+	})
+})
+//function
+function pressure(e) {
+	let currentLevel = Number(e.target.style.opacity);
+	if (currentLevel == 1) return;
+	e.target.style.opacity = currentLevel += 0.1;
+}
+
+
+reset.addEventListener('click', start);
 function start(a) {
 	document.body.removeChild(container);
 	do {
@@ -57,77 +149,5 @@ function start(a) {
 	container.style.gridTemplateColumns = "repeat(" + a + ", 1fr)";
 	container.style.gridTemplateRows = "repeat(" + a + ", 1fr)";
 }
-reset.addEventListener('click', start);
-toggle.addEventListener('click', (e) => {
-	var gridItem = document.querySelectorAll('.gridItem');
-	gridItem.forEach((div) => {
-		div.classList.toggle('borderGrid')
-	})
-})
-rainbow.addEventListener('click', (e) => {
-	randomColor = true;
-	blackColor = false;
-	eraseColor = false;
-	var gridItem = document.querySelectorAll('.gridItem');
-	gridItem.forEach((div) => {
-		div.addEventListener('mouseenter', random);
-	})
-})
-erase.addEventListener('click', (e) => {
-	randomColor = false;
-	blackColor = false;
-	eraseColor = true;
-	var gridItem = document.querySelectorAll('.gridItem');
-	gridItem.forEach((div) => {
-		div.addEventListener('mouseenter', eraseElement)
-	})
-})
-pencil.addEventListener('click', (e) => {
-	randomColor = false;
-	blackColor = true;
-	eraseColor = false;
-	var gridItem = document.querySelectorAll('.gridItem');
-	gridItem.forEach((div) => {
-		div.addEventListener('mouseenter', backToBlack)
-	})
-})
-fine.addEventListener('click', (e) => {
-	var gridItem = document.querySelectorAll('.gridItem');
-	gridItem.forEach((div) => {
-		div.addEventListener('mouseenter', pressure)
-	})
-})
 
-function backToBlack(e) {
-	if (blackColor === false) {
-		return
-	} else {
-		this.style.backgroundColor = 'black';
-	}
-}
 
-function eraseElement(e) {
-	if (eraseColor === false) {
-		return
-	} else {
-		this.style.backgroundColor = '#fff';
-	}
-}
-
-function random(e) {
-	if (randomColor === false) {
-		return
-	} else {
-		this.style.backgroundColor = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
-	}
-}
-
-function colorIn(e) {
-	this.classList.add("change");
-}
-
-function pressure(e) {
-	let currentLevel = Number(e.target.style.opacity);
-	if (currentLevel == 1) return;
-	e.target.style.opacity = currentLevel += 0.1;
-}
